a34ac9d3477d9d20758895759bd878e5f82fa3c4
Jaideep Singh <ee26btech11213@iith.ac.in>
2026-09-23 15:54:59 +0530

    Added days.c

--- LAb7/days.c
+++ LAb7/days.c
@@ -1,1 +1,24 @@
-#include <stdio.h>
+##include <stdio.h>
+
+int daysElapsed(int day, int month);
+
+int main() {
+    int day, month;
+    scanf(" %d %d", &day, &month);
+    printf(" %d", daysElapsed(day,month));
+
+    return 0;
+}
+
+int daysElapsed(int day, int month) {
+    int time=0;
+    for(int i=1;i<month;i++){
+        if(i==1 || i==3 || i==5 || i==7 || i==8 || i==10)
+        time = time + 31;
+        
+        else if(i==4 || i==6|| i==9 || i==11) time = time+30;
+        else if(i==2) time = time + 28;
+    }
+    time = time + day;
+    return time;
+}include <stdio.h>
